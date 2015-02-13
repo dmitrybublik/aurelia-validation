52640c617055b583a8b9dfc75352b76c36fb08a7
janvanderhaegen <van.der.haegen.j@gmail.com>
2015-02-13 10:06:22 -0500

    moving src to 'src' folder

--- validation.js
+++
@@ -1,7 +0,0 @@
-export * from './validation/validation';
-import {ValidateAttachedBehavior} from './validation/validation'
-
-export function install(aurelia)
-{
-	aurelia.withResources([ValidateAttachedBehavior]);
-}
